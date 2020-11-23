eaa03d5803b68ac12d3e4c9286e5ac92d5b36c21
paulndungu <p.ndungu.kara@gmail.com>
2020-11-23 12:49:37 +0300

    finished simple functions

---
+++ src/simple function.kt
@@ -0,0 +1,19 @@
+fun addition(n:Int,n1:Int,n2:Double): Double {
+
+    var addition=n+n1+n2
+    return addition
+}
+fun show(word:String="this is gonna be a sunny day"):Unit{
+
+    println("$word")
+}
+fun main(){
+
+    var add=10.0+20.0+30.0
+    println(add)
+    var add2=40.5+50.5+60.5
+    println(add2)
+
+    show("Damn")
+    show()
+}
