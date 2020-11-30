7e17a156d6c379c0b4d5132c1ca5286a01884c59
paulndungu <p.ndungu.kara@gmail.com>
2020-11-30 11:50:18 +0300

    done with local and global variables.kt

---
+++ src/local and global variables.kt
@@ -0,0 +1,11 @@
+var globalusername="simonpeter"  //global var
+fun name():Unit{
+
+    println(globalusername)
+}
+fun main(){
+
+    var name="simon" //local var
+    println(name)
+    println(globalusername)
+}
